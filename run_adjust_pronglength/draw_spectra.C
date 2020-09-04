fafe34ae22263d190a965a565aed10d3d4100b45
Shaowei <swyx2017@gmail.com>
2020-09-04 11:51:46 -0500

    autoupdate

--- run_adjust_pronglength/draw_spectra.C
+++ run_adjust_pronglength/draw_spectra.C
@@ -18,21 +18,30 @@ using namespace ana;
 
 void draw_spectra(){
 
-  TFile inFile("/nova/ana/users/wus/root_files/FD_FHC_spectra.root");
+  TFile inFile_0("/nova/ana/users/wus/root_files/FD_FHC_spectra.root");
+  TFile inFile_5("/nova/ana/users/wus/root_files/FD_FHC_spectra_x.root");
 
   // Load the spectrum...
-  std::unique_ptr<Spectrum> nhit = Spectrum::LoadFrom(inFile.GetDirectory("subdir_muE_spectra"));
+  std::unique_ptr<Spectrum> spect_0 = Spectrum::LoadFrom(inFile_0.GetDirectory("subdir_muE_spectra"));
+  std::unique_ptr<Spectrum> spect_5 = Spectrum::LoadFrom(inFile_5.GetDirectory("subdir_muE_spectra"));
 
 
 
   //
   // Plot the histo...
   //  
-  TCanvas *cNHit = new TCanvas("cNHit","plot muE_spectra",1000,800);
-  cNHit->cd();
-  TH1D *hNHit = nhit->ToTH1(nhit->POT());
-  hNHit->SetLineWidth(2);
-  hNHit->SetLineColor(kGreen+2);
-  hNHit->Draw("hist");
+  TCanvas *canvas_0 = new TCanvas("canvas_0","plot muE_spectra",1000,800);
+  canvas_0->cd();
+
+  TH1D *TH1D_0 = spect_0->ToTH1(spect_0->POT());
+  TH1D *TH1D_5 = spect_5->ToTH1(spect_5->POT());
+
+  TH1D_0->SetLineWidth(2);
+  TH1D_0->SetLineColor(kGreen+2);
+  TH1D_0->Draw("hist_0");
+
+  TH1D_5->SetLineWidth(2);
+  TH1D_5->SetLineColor(kRed+2);
+  TH1D_5->Draw("hist_5");
 
 }
