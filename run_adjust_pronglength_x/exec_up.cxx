01378d45028c03408f896949e87bb898c5efb389
Shaowei <swyx2017@gmail.com>
2020-09-28 01:20:40 -0500

    autoupdate

--- run_adjust_pronglength_x/exec_up.cxx
+++ run_adjust_pronglength_x/exec_up.cxx
@@ -1,4 +1,25 @@
 #include "select_abs_up.h"
+#include "CAFAna/Core/Binning.h"
+#include "CAFAna/Cuts/Cuts.h"
+#include "CAFAna/Core/Spectrum.h"
+#include "CAFAna/Core/SpectrumLoader.h"
+#include "CAFAna/Vars/Vars.h"
+
+#include "StandardRecord/Proxy/SRProxy.h"
+
+#include "TCanvas.h"
+#include "TH2.h"
+
+#include "TensorFlowEvaluator/LSTME/cafana/LSTMEVar.h"
+
+#include "CAFAna/Cuts/NumuCuts.h"
+#include "CAFAna/Cuts/NumuCuts2018.h"
+#include "TFile.h"  
+#include "TH1.h"   
+
+#include "CAFAna/Core/ISyst.h"
+
+#include <string> 
 void exec_up(){
     select_abs_and_mode_up(111, 0);
 }
--- run_adjust_pronglength_x/mode_cut.h
+++ run_adjust_pronglength_x/mode_cut.h
@@ -1,6 +1,24 @@
+#include "CAFAna/Core/Binning.h"
 #include "CAFAna/Cuts/Cuts.h"
+#include "CAFAna/Core/Spectrum.h"
+#include "CAFAna/Core/SpectrumLoader.h"
 #include "CAFAna/Vars/Vars.h"
+
 #include "StandardRecord/Proxy/SRProxy.h"
+
+#include "TCanvas.h"
+#include "TH2.h"
+
+#include "TensorFlowEvaluator/LSTME/cafana/LSTMEVar.h"
+
+#include "CAFAna/Cuts/NumuCuts.h"
+#include "CAFAna/Cuts/NumuCuts2018.h"
+#include "TFile.h"  
+#include "TH1.h"   
+
+#include "CAFAna/Core/ISyst.h"
+
+#include <string> 
 
 // QE, RES, DIS, Coh, MEC cut
 
