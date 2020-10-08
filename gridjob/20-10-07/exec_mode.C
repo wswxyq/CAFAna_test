8a3145cc2fd6372cad1b1339ce38aff95c2da848
Shaowei <swyx2017@gmail.com>
2020-10-08 11:46:27 -0500

    autoupdate

--- gridjob/20-10-07/exec_mode.C
+++ gridjob/20-10-07/exec_mode.C
@@ -128,7 +128,7 @@ using namespace ana;
         auto &png = sr->vtx.elastic.fuzzyk.png;
         for (size_t i = 0; i < png.size(); i++) {
           // png[i].len // this will give you lenght of the prong number i
-          if (abs(png[i].truth.pdg)!==input_pdg)
+          if (abs(png[i].truth.pdg)!=input_pdg)
           {
             png[i].len *= shift_ratio; // 0.01 = 1%
             png[i].bpf.muon.energy *= shift_ratio;
@@ -147,7 +147,7 @@ using namespace ana;
         }
         auto &png2d = sr->vtx.elastic.fuzzyk.png2d;
         for (size_t i = 0; i < png2d.size(); i++) {
-          if (abs(png2d[i].truth.pdg)!==input_pdg)
+          if (abs(png2d[i].truth.pdg)!=input_pdg)
           {
             png2d[i].len *= shift_ratio; 
           }
