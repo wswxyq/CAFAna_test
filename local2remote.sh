c40ed3b53d998bb95f8c040526e77bd64ce7fe26
Shaowei <swyx2017@gmail.com>
2020-10-09 01:26:54 -0500

    autoupdate

--- local2remote.sh
+++ local2remote.sh
@@ -1,1 +1,4 @@
+# Used as temporary sync tool
+# Do not use rsync to pull remote to local
+# rsync will not delete files that do not exist in destination but source
 rsync -chavzP --stats . wus@novagpvm05.fnal.gov:/nova/app/users/wus/CAFAna_test --exclude=.git
