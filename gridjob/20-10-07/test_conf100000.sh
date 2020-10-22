6ba4d3702e472692ebd5bcbee83398b5dac6661c
Shaowei <swyx2017@gmail.com>
2020-10-22 00:59:01 -0500

    batch draw

---
+++ gridjob/20-10-07/test_conf100000.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RESULT_DIR="./results/"
+RESULT_FILE="spectra.root"
+
+SCRIPT=( exec_mode.C )
+NJOBS=20
+
+JOB_OPTIONS=(
+    --rel     "S20-04-03"
+    --testrel "/nova/app/users/wus/fake_release"
+)
+
+OUTDIR="/pnfs/nova/scratch/users/wus/2020-10-07_wu_test"
+
+MODE_VALUES=( 100000 )
+PDG_VALUES=( 111 211 2212 2112 11 13 15 -13)
+PM_VALUES=( 1 -1 )
+
+declare -A CONFIGS
+
+for mode in "${MODE_VALUES[@]}"
+do
+    for pdg in "${PDG_VALUES[@]}"
+    do
+        for pm in "${PM_VALUES[@]}"
+        do
+            CONFIGS["${mode}_${pdg}_${pm}"]="${mode} ${pdg} ${pm}"
+        done
+    done
+done
+
